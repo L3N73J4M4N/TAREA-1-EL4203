8058ce272375a5cfa0168a1bc92c97d4968e9cdd
Matías Carvajal Pérez <91276226+L3N73J4M4N@users.noreply.github.com>
2024-09-15 21:41:01 -0300

    Add files via upload

---
+++ caminos.py
@@ -0,0 +1,97 @@
+import numpy as np
+from time import time
+
+
+# decorador para obtener tiempo [ms] que tarda una
+# función en terminar de ejecutarse
+def decorator(func):
+    def get_time(self, *args):
+        t0 = time()
+        func(self, *args)
+        tf = time()
+        return 1000 * (tf - t0)
+    return get_time
+
+
+# La clase Caminos recibe como argumento las dimensiones de la PCB
+# donde N es el número de columnas y M el de filas
+class Caminos:
+    def __init__(self, n: int, m: int):
+        self.pcb = np.zeros((m, n))  # matriz de M x N
+        self.n, self.m = n, m  # dimensiones de la PCB
+        self.A, self.B = [0, 0], [n - 1, m - 1]  # puntos A y B
+        self.roads = 0
+
+    # para encontrar los caminos, podemos aprovechar la recursividad
+    # permitiendo hacer recorridos distintos dependiendo del
+    # sentido del movimiento
+    def recursive(self):
+        self.roads = 0  # partimos de cero recorridos encontrados
+
+        # determina el camino desde un punto point considerando que
+        # ya ha pasado todos los puntos en visited para no devolverse
+        # visited se va copiando para ir buscando por rama
+        # y que se permita encontrar más de un recorrido
+        def road(point, visited):
+            i, j = point
+            visited.append([i, j])  # visitamos [i, j]
+            if [i, j] == self.B:  # llegamos a B
+                self.roads += 1  # aumentamos el contador
+            else:
+                # vamos a la derecha
+                if [i + 1, j] not in visited and i + 1 < self.n:
+                    road([i + 1, j], visited.copy())
+                # vamos a la izquierda
+                if [i - 1, j] not in visited and i - 1 >= 0:
+                    road([i - 1, j], visited.copy())
+                # vamos hacia abajo
+                if [i, j + 1] not in visited and j + 1 < self.m:
+                    road([i, j + 1], visited.copy())
+                # vamos hacia arriba
+                if [i, j - 1] not in visited and j - 1 >= 0:
+                    road([i, j - 1], visited.copy())
+
+        road(self.A, [])  # partimos de A y no hemos pasado por ningún punto
+        return self.roads
+
+    # algoritmo iterativo con idea similar al anterior, donde se van
+    # registrando los estados actuales. Termina cuando se terminan los estados
+    def iterative(self):
+        self.roads = 0  # partimos de cero recorridos encontrados
+        # states indica el estado [point = [i, j], visitados = [[i, j], ... ]]
+        states = [[self.A, [self.A]]]  # partimos de A y solo hemos pasado por A
+        while len(states) > 0:
+            [i, j], visited = states.pop()  # vemos el último estado
+            if [i, j] == self.B:  # llegamos a B
+                self.roads += 1  # aumentamos el contador
+            # vamos a la derecha
+            if [i + 1, j] not in visited and i + 1 < self.n:
+                states.append([[i + 1, j], visited + [[i + 1, j]]])
+            # vamos a la izquierda
+            if [i - 1, j] not in visited and i - 1 >= 0:
+                states.append([[i - 1, j], visited + [[i - 1, j]]])
+            # vamos hacia abajo
+            if [i, j + 1] not in visited and j + 1 < self.m:
+                states.append([[i, j + 1], visited + [[i, j + 1]]])
+            # vamos hacia arriba
+            if [i, j - 1] not in visited and j - 1 >= 0:
+                states.append([[i, j - 1], visited + [[i, j - 1]]])
+        return self.roads
+
+    # retorna el número de caminos que tiene la clase a través
+    # del algoritmo iterativo en caso de True. En cambio,
+    # si fuera False, utiliza el algoritmo recursivo
+    @ decorator
+    def num_roads(self, iterative):
+        return self.iterative() if iterative is True else self.recursive()
+
+
+if __name__ == '__main__':
+    # verificamos casos simples
+    assert Caminos(3, 3).recursive() == Caminos(3, 3).iterative() == 12
+    assert Caminos(2, 2).recursive() == Caminos(2, 2).iterative() == 2
+    # verificamos que es conmutativo
+    assert Caminos(3, 2).iterative() == Caminos(2, 3).recursive() == 4
+    assert Caminos(2, 3).iterative() == Caminos(3, 2).recursive() == 4
+    # verificamos un caso complejo
+    assert Caminos(5, 5).iterative() == Caminos(5, 5).recursive()
---
+++ graphs.py
@@ -0,0 +1,45 @@
+from caminos import Caminos
+import matplotlib.pyplot as plt
+
+# obtención de datos para la gráfica
+dims = range(2, 6)
+x, it, rec = [], [], []
+for d1 in dims:
+    for d2 in dims:
+        x.append(f'({d1}, {d2})')
+        it.append(Caminos(d1, d2).num_roads(True))
+        rec.append(Caminos(d1, d2).num_roads(False))
+
+max_time = max(max(rec), max(it)) + 30
+# gráfico de ambos algoritmos
+plt.figure(figsize=(10, 6))
+plt.bar(x, it, color='coral', label='Iteración')
+plt.bar(x, rec, color='royalblue', label='Recursión')
+plt.ylim([0, max_time])
+plt.ylabel('tiempo [ms]')
+plt.xlabel('dimensiones de matriz [N x M]')
+plt.title('Tiempo que tardan los algoritmos en función de las dimensiones.')
+plt.legend()
+plt.savefig('grafico_ambos_algoritmos.svg')
+plt.show()
+
+# gráfico de algoritmo iterativo
+plt.figure(figsize=(10, 6))
+plt.bar(x, it, color='coral')
+plt.ylabel('tiempo [ms]')
+plt.ylim([0, max_time])
+plt.xlabel('dimensiones de matriz [N x M]')
+plt.title('Tiempo que tarda algoritmo iterativo en función de las dimensiones.')
+plt.savefig('grafico_iteracion.svg')
+plt.show()
+
+# gráfico de algoritmo recursivo
+plt.figure(figsize=(10, 6))
+plt.bar(x, rec, color='royalblue')
+plt.ylabel('tiempo [ms]')
+plt.ylim([0, max_time])
+plt.xlabel('dimensiones de matriz [N x M]')
+plt.title('Tiempo que tarda algoritmo recursivo en función de las dimensiones.')
+plt.savefig('grafico_recursion.svg')
+plt.show()
+
